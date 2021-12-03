7ae68d3dcc62cfd12e1af1c3da53d5de71ca1e49
phuduy2610 <phuduy2610@gmail.com>
2021-12-03 19:44:27 +0700

    - Điều chỉnh cho tile bắt đầu từ góc trên trái camera
    + Thêm hàm tìm điểm trên trái
    = Update lại hàm đặt tile

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,9 +8,26 @@ public class LevelManager : MonoBehaviour
     private GameObject[] pathTiles;
     private int width = 5;
     private int height = 5;
+
+    //Object camera dùng để lấy kích thước màn hình
+    Camera cameraView;
+
+    //Điểm bắt đầu đặt tile lên
+    Vector3 startPoint;
+
+    // Tạo một property trả về size của một tile ( hình vuông nên chỉ cần lấy x)
+    public float TileSize
+    {
+        get
+        {
+            return pathTiles[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
+        cameraView = Camera.main;
+        startPoint = GetTopLeftPointOfCamera();
         CreateLevel(1);
     }
 
@@ -20,19 +37,29 @@ public class LevelManager : MonoBehaviour
 
     }
 
+    //Hàm tạo Level
     private void CreateLevel(int levelIndex)
     {
+        PlaceTile(startPoint);
+    }
+
+    //Hàm đặt các Tile lên màn hình
+    private void PlaceTile(Vector3 startPoint)
+    {
         //Lấy size của một ô tile ( chỉ lấy cái đầu do bằng nhau hết)
-        float tileWidth = pathTiles[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-        float tileHeight = pathTiles[0].GetComponent<SpriteRenderer>().sprite.bounds.size.y;
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
                 GameObject currentTile = Instantiate(pathTiles[Random.Range(0, pathTiles.Length)]);
                 //Ô tiếp theo thì kế bên ô hiện tại nên += tích của hệ số i và j
-                currentTile.transform.position = new Vector3(currentTile.transform.position.x + j * tileWidth, currentTile.transform.position.y - i * tileHeight, 0);
+                currentTile.transform.position = new Vector3(startPoint.x + j * TileSize, startPoint.y - i * TileSize, 0);
             }
         }
+    }
+
+    private Vector3 GetTopLeftPointOfCamera()
+    {
+        return cameraView.ViewportToWorldPoint(new Vector3(0, 1, cameraView.nearClipPlane));
     }
 }
