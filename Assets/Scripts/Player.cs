11b3a30f2a7cedbe21963ef73369758444ddbbb7
ninonesix <mninonesix@gmail.com>
2021-12-15 21:58:54 +0700

    Thêm prefab temp_main, gate

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : Entity
 {
+    private Vector2 direction = Vector2.zero;
+    private Rigidbody2D physicBody;
     protected override void Attack()
     {
         throw new System.NotImplementedException();
     }
 
+    // Move player on screen. 
+    // Precondition: none
+    // Postcondition: none
     protected override void Move()
     {
-        throw new System.NotImplementedException();
+        if (direction.x < 0)
+        {
+            transform.localScale = new Vector2(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else if (direction.x > 0)
+        {
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        physicBody.transform.position += new Vector3(direction.x, direction.y) * Time.deltaTime * MoveSpeed;
     }
+
+    private void Awake()
+    {
+        physicBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    // Get player movement input. Must only be called by player input. 
+    // Precondition: none
+    // Postcondition: none
+    public void GetMovementInput(InputAction.CallbackContext callbackContext)
+    {
+        direction = callbackContext.ReadValue<Vector2>();
+    }
+
+    // example
+    // Sorts an array into ascending order.
+    // Precondition: anArray is an array of num integers and 1 <= num <= MAX_ARRAY,
+    // where MAX_ARRAY is a global constant that specifi es the maximum size of anArray .
+    // Postcondition: anArray[0]<= anArray[1]<= … <= anArray[num - 1];
+    // num is unchanged.
 }
